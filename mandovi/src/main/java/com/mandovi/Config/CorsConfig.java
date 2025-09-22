325691caa0080bc3cfefd9c0ade5697fcb2fd135
arunmandovi <arunmandovi86@gmail.com>
2025-09-22 17:29:52 +0530

    url added

--- mandovi/src/main/java/com/mandovi/Config/CorsConfig.java
+++ mandovi/src/main/java/com/mandovi/Config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500",
                                         "http://localhost:5500",
-                                        "http://localhost:3000")
+                                        "http://localhost:3000",
+                                        "https://mandovi4.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
