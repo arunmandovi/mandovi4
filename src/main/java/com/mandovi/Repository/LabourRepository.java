f3448310aa28946005224f4f9bdd25ada724192a
arunmandovi <arunmandovi86@gmail.com>
2025-11-24 17:51:33 +0530

    Labour repo updated

--- src/main/java/com/mandovi/Repository/LabourRepository.java
+++ src/main/java/com/mandovi/Repository/LabourRepository.java
@@ -33,11 +33,11 @@ public interface LabourRepository extends JpaRepository<Labour, Integer> {
             SELECT new com.mandovi.DTO.LabourSummaryDTO(
             l.city,
             null,
-            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ),
-            SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ),
-            (SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ) -
-            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END )) * 100 /
-            NULLIF(SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ), 0),
+            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ),
+            SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ),
+            (SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ) -
+            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END )) * 100 /
+            NULLIF(SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ), 0),
             SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType = 'BODYSHOP' THEN l.labour ELSE 0 END ),
             SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType = 'BODYSHOP' THEN l.labour ELSE 0 END ),
             (SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType = 'BODYSHOP' THEN l.labour ELSE 0 END ) -
@@ -92,27 +92,27 @@ public interface LabourRepository extends JpaRepository<Labour, Integer> {
             SELECT new com.mandovi.DTO.LabourSummaryDTO(
             l.city,
             l.branch,
-            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ),
-            SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR') THEN l.labour ELSE 0 END ),
+            SUM(CASE WHEN l.financialYear = '2024-2025' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ),
+            SUM(CASE WHEN l.financialYear = '2025-2026' AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO') THEN l.labour ELSE 0 END ),
             CASE
                 WHEN SUM(CASE WHEN l.financialYear = '2025-2026'
-                AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR')
+                AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO')
                 THEN l.labour ELSE 0 END) = 0
                 THEN 0
                 ELSE
                     (
                         (SUM(CASE WHEN l.financialYear = '2025-2026'
-                         AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR')
+                         AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO')
                          THEN l.labour ELSE 0 END)
                          -
                          SUM(CASE WHEN l.financialYear = '2024-2025'
-                         AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR')
+                         AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO')
                          THEN l.labour ELSE 0 END)
                         ) * 100
                         /
                         NULLIF(
                             SUM(CASE WHEN l.financialYear = '2024-2025'
-                            AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR')
+                            AND l.loadType IN ('OTHERS','FREE SERVICE','PMS','RR', 'NO')
                             THEN l.labour ELSE 0 END),
                         0)
                     )
